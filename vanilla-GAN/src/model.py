01f02a2182439fbd7fe2f7063cf0961f545c0ba1
marload <rladhkstn8@gmail.com>
2020-02-08 16:48:28 +0900

    implementation vanilla gan

--- vanilla-GAN/src/model.py
+++ vanilla-GAN/src/model.py
@@ -4,7 +4,7 @@ from tensorflow.keras import layers
 
 def make_discriminaor():
     return tf.keras.Sequential([
-        layers.Dense(128,  activation='relu', input_shape=(784)),
+        layers.Dense(128,  activation='relu', input_shape=(784,)),
         layers.Dense(256, activation='relu'),
         layers.Dense(1, activation='sigmoid')
     ])
@@ -12,7 +12,7 @@ def make_discriminaor():
 
 def make_generator():
     return tf.keras.Sequential([
-        layers.Dense(128, activation='relu', input_shape=(100)),
-        layers.Dense(256, activation='relu')
+        layers.Dense(128, activation='relu', input_shape=(100,)),
+        layers.Dense(256, activation='relu'),
         layers.Dense(784, activation='tanh')
     ])
--- vanilla-GAN/src/utils.py
+++ vanilla-GAN/src/utils.py
@@ -3,11 +3,11 @@ import tensorflow as tf
 loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 
-def d_loss(real_output, fake_output):
+def compute_disc_loss(real_output, fake_output):
     real_loss = loss_object(tf.ones_like(real_output), real_output)
     fake_loss = loss_object(tf.zeros_like(fake_output), fake_output)
     return real_loss + fake_loss
 
 
-def g_loss(fake_output):
+def compute_gen_loss(fake_output):
     return loss_object(tf.ones_like(fake_output), fake_output)
